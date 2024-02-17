db878f55e81c4c932429af8f44dc9c7198758a79
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-02-17 22:56:52 +0530

    Connect MongoDB with personalBlog

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express();
 const port = 3000;
+
+const connectURL = process.env.MONGO;
+
+mongoose
+    .connect(connectURL)
+    .then(()=>{
+        console.log("MongoDB is connected to personalBlog");    
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
 
 app.listen(port, ()=>{
     console.log(`server is up and running on port ${port}`);
