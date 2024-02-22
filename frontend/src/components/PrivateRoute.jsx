a4684b71647ee20e8f55c11e41d836d9b4960894
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-02-22 18:26:02 +0530

    private dashboard

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import Dashboard from '../pages/Dashboard';
-import SignIn from '../pages/SignIn';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 const PrivateRoute = () => {
   const {currentUser} = useSelector(state=>state.user);
   return (
-    currentUser ? <Outlet /> : <SignIn />
+    currentUser ? <Outlet /> : <Navigate to='sign-in' />
   )
 }
 
