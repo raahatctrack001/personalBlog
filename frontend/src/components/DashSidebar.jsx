1ecb4b4078a9cb94d12a5de7d882cbf41a355f24
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-02-22 22:39:14 +0530

    Complete side bar dashboard

---
+++ frontend/src/components/DashSidebar.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import {Sidebar} from 'flowbite-react'
+import {HiUser,  HiArrowSmRight} from 'react-icons/hi'
+import { Link, useLocation } from 'react-router-dom'
+const DashSidebar = () => {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+
+    useEffect(()=>{
+        const searchParams = new URLSearchParams(location.search);
+        const tabFromURL = searchParams.get('tab');
+        if(tabFromURL){
+            setTab(tabFromURL);
+        }
+    }, [location.search]);
+  return (
+    <Sidebar className='w-full md:w-56'>
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+            <Link to='/dashboard?tab=profile'>
+                <Sidebar.Item active={tab==='profile'} icon={HiUser} label='user' labelColor='dark' >
+                    profile
+                </Sidebar.Item>
+            </Link>
+                <Sidebar.Item icon={HiArrowSmRight} >
+                    Sign Out
+                </Sidebar.Item>
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
+
+export default DashSidebar
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashSidebar from '../components/DashSidebar'
+import DashProfile from '../components/DashProfile';
 
 const Dashboard = () => {
+  //localhost:5173/dashboard?tab=profile
+  const location = useLocation();
+  // location = {pathname: '/dashboard', search: '?tab=profile', hash: '', state: null, key: 'default'}
+
+  const [tab, setTab] = useState('');
+
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromURL = urlParams.get('tab');
+
+    if(tabFromURL){
+      setTab(tabFromURL);
+    }
+    
+  }, [location.search])
+ 
   return (
-    <div>Dashboard</div>
-  )
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div>
+        <DashSidebar />
+
+      </div>
+
+      {tab==='profile' && <DashProfile />}
+    </div>
+  );
 }
 
 export default Dashboard
